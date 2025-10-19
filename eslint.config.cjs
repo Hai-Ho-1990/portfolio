c15af97e094d95cbeb26a9036e56658e7db6e9f0
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-10-19 17:33:06 +0200

    Migrate to ESLint flat config (eslint.config.cjs) and enable Prettier as rule

---
+++ eslint.config.cjs
@@ -0,0 +1,44 @@
+module.exports = [
+  // Global ignores
+  { ignores: ['node_modules/**', 'dist/**', 'build/**'] },
+
+  // Apply to TS/TSX/JS/JSX files
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
+      prettier: require('eslint-plugin-prettier')
+    },
+    rules: {
+      // Integrate Prettier formatting as an ESLint rule
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          tabWidth: 2,
+          useTabs: false,
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'es5',
+          bracketSpacing: true,
+          arrowParens: 'always'
+        }
+      ],
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+]
