edc6ac1c0e25bb03a5eb218dc6db622a9982197d
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-10-19 17:49:46 +0200

    Fix lint: add missing semicolons and format TSX files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function App(): JSX.Element {
   return (
@@ -32,5 +32,5 @@ export default function App(): JSX.Element {
         <small>© {new Date().getFullYear()} Hai Ho</small>
       </footer>
     </div>
-  )
+  );
 }
