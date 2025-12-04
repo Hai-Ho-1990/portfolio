b73248656647c8971871b9a0d67bc01e94437717
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-12-04 18:53:32 +0100

    small changes

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ✅ Middleware
-app.use(cors({ origin: 'http://localhost:5174' })); // CORS för frontend
+app.use(cors({ origin: 'https://portfolio-pwex.onrender.com' }));
+// CORS för frontend
 app.use(express.json({ limit: '1mb' }));
 
 // Supabase setup
