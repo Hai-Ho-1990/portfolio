257e2070b9ed88c739039cdb2fe9f08c905ad908
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-12-04 19:13:34 +0100

    small changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
+
+// Ensure root is project root, not src/
+const projectRoot = path.resolve(__dirname);
 
 export default defineConfig({
-  root: './',
+  root: projectRoot,
   plugins: [react()],
   resolve: {
     alias: {
