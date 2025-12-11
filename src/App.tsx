aecca722ba46b2025e09682ea90175d8e60bdbda
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-12-11 14:19:23 +0100

    Temp rename for case fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import About from './components/About';
 import Projects from './pages/Projects';
 import NotFound from './pages/NotFound';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/temp';
 
 function Layout(): JSX.Element {
   return (
